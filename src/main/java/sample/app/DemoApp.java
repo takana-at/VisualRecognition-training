af88e51a3effe00aab82342331ae0923e6fe24b5
takana-at <3aun.bmf.397a@gmail.com>
2017-02-13 11:05:28 +0900

    DemoApp.java変更

--- src/main/java/sample/app/DemoApp.java
+++ src/main/java/sample/app/DemoApp.java
@@ -4,6 +4,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DemoApp {
 	public static App app = new App();
@@ -25,13 +28,41 @@ public class DemoApp {
 		 * class="thumbnailBox">】タグに格納されている
 		 * 2017/2/8段階では、【日本の女優一覧】というページから女優リストを取得する
 		 */
-
+		
+		
 		String app_html = app.getHTML(site_url);
 		// System.out.println(app_html);
 		/*
 		 * TrimTextメソッドの実装 HTMLタグから ページ全体の有名人に関する情報は、【<div
 		 * class="thumbnailBox">】タグの内容を文字列で抽出
 		 */
+		
+///*2017/2/10追加	*/		
+//		/*	現時点で未実装
+//		 * 2017/2/10段階では、【日本の女優一覧】に該当するものを1~7ページまですべてまわす
+//		 * categoryという変数で、女優一覧に該当するものを何ページあるか判定
+//		 * 
+//		 *　 
+//		 */
+//		String category = app.TrimText(app_html,new String[]{"pagenavi","<a"} ,"</div>" );
+//		String unshaped_end_page = category.substring(0,category.lastIndexOf("<a"));
+//		int end_page = Integer.parseInt(unshaped_end_page.substring(unshaped_end_page.lastIndexOf("\">")+2,unshaped_end_page.lastIndexOf("</a")));
+//		
+//			int num = 1;
+//			while(true){
+//			int site_url_num = Integer.parseInt(site_url.substring(site_url.lastIndexOf("_")+1,site_url.lastIndexOf(".")));
+//			site_url = site_url.replace(String.valueOf(site_url_num), String.valueOf(num));
+//			if(end_page < num){
+//				break;
+//			}
+//			
+//			System.out.println(site_url);
+//			site_url_num ++;
+//			num++;
+//		}
+///*2017/2/10追加(終了位置)	*/
+
+		
 		String person_group_info = app.TrimText(app_html, new String[] { "thumbnailBox_list", ">" }, "<span class=");
 		// System.out.println(person_group_info);
 
@@ -65,10 +96,10 @@ public class DemoApp {
 		String person_name = app.TrimText(str, new String[] { "</a>", ">" }, "</a>");
 		String person_image_url = app.TrimText(str, new String[] { "<img", "src=\"" }, "\"");
 		String person_detail_page = person_search_url + app.TrimText(str, new String[] { "<a", "href=\"" }, "\"");
-
+/*
 		shapeImg(person_name, person_image_url);
-		produceText(person_name, person_image_url, person_detail_page);
-
+		produceText(person_name, person_image_url, person_detail_page);		
+*/
 		// try {
 		// FileOutputStream output = new FileOutputStream("images/" +
 		// person_name + ".jpg");
@@ -118,5 +149,32 @@ public class DemoApp {
 		}
 		return null;
 	}
-
+//	/*2017/2/10追加(開始位置)	*/		
+//	/*	URL存在確認メソッド	*/
+//    public static boolean isExistURL(String urlStr) {
+//        URL url;
+//        int status = 0;
+//        try {
+//            url = new URL(urlStr);
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//            conn.setRequestMethod("HEAD");
+//            conn.connect();
+//            status = conn.getResponseCode();
+//            conn.disconnect();
+//        } catch (MalformedURLException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//
+//        if (status == HttpURLConnection.HTTP_OK) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//	/*2017/2/10追加(終了位置)	*/	
+	
 }
