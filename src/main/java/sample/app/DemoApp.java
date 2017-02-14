914e8e359dcfc10899fe86e32fe7fe14833575da
takana-at <3aun.bmf.397a@gmail.com>
2017-02-14 11:45:41 +0900

    検索結果のすべてのページのデータをとれるようwhile文の追加 retrievePersonsメソッドの実装により、検索結果のページの各データを取得できるように実装 URLの存在確認のためのisExistURLメソッド削除

--- src/main/java/sample/app/DemoApp.java
+++ src/main/java/sample/app/DemoApp.java
@@ -11,80 +11,61 @@ import java.net.URL;
 public class DemoApp {
 	public static App app = new App();
 
-
 	public static void main(String[] args) {
 
 		if (args == null || args.length != 2){
 			throw new IllegalArgumentException("no args...");
 		}
+		
+		/* トレーニングデータとして抽出するURLを定義する */
 		String site_url = args[0];
-		/* トレーニングデータとして抽出するURLをperson_search_urlという変数で定義する */
 		String person_search_url = args[1];
 
-		/*
-		 * 【原寸画像検索】というサイトのページからHTMLタグの抽出 抽出したHTMLから、さらに有名人に関する情報を抽出する
-		 * 具体的に抽出する項目は、【有名人名】と【有名人の画像のURL】である そのページ全体の有名人に関する情報は、【<div
-		 * class="thumbnailBox_list">】タグに格納されている また、有名人1人1人の情報は、【<div
-		 * class="thumbnailBox">】タグに格納されている
-		 */
-		
-		
 		String app_html = app.getHTML(site_url);
-		// System.out.println(app_html);
-		/*
-		 * TrimTextメソッドの実装 HTMLタグから ページ全体の有名人に関する情報は、【<div
-		 * class="thumbnailBox">】タグの内容を文字列で抽出
+
+		/*2017/2/10追加			
+		 * categoryという変数で、有名人の検索結果は何ページあるか判定し、検索結果のページ数をend_pageという変数におき、検索結果すべての内容を抽出する
 		 */
-		
-///*2017/2/10追加	*/		
-//		/*	現時点で未実装
-//		 * 2017/2/10段階では、【日本の女優一覧】に該当するものを1~7ページまですべてまわす
-//		 * categoryという変数で、女優一覧に該当するものを何ページあるか判定
-//		 * 
-//		 *　 
-//		 */
-//		String category = app.TrimText(app_html,new String[]{"pagenavi","<a"} ,"</div>" );
-//		String unshaped_end_page = category.substring(0,category.lastIndexOf("<a"));
-//		int end_page = Integer.parseInt(unshaped_end_page.substring(unshaped_end_page.lastIndexOf("\">")+2,unshaped_end_page.lastIndexOf("</a")));
-//		
-//			int num = 1;
-//			while(true){
-//			int site_url_num = Integer.parseInt(site_url.substring(site_url.lastIndexOf("_")+1,site_url.lastIndexOf(".")));
-//			site_url = site_url.replace(String.valueOf(site_url_num), String.valueOf(num));
-//			if(end_page < num){
-//				break;
-//			}
-//			
-//			System.out.println(site_url);
-//			site_url_num ++;
-//			num++;
-//		}
-///*2017/2/10追加(終了位置)	*/
-
-		
-		String person_group_info = app.TrimText(app_html, new String[] { "thumbnailBox_list", ">" }, "<span class=");
-		// System.out.println(person_group_info);
-
-		/*
-		 * TrimTextNextメソッドの実装
-		 * TrimTextメソッドで抽出した文字列から、有名人1人1人の情報は、【<a>】タグの内容を文字列で抽出 persons_info !=
-		 * nullのとき、[0]番目の配列には該当する文字列に有名人1人に関する情報を、
-		 * [1]番目の配列にはそれ以降の有名人全員に関する情報をを返す
-		 * while文の中では、1回目のループでperson_group_infoから有名人1人の情報を文字列で検索
-		 * 2回目以降のループで、persons_info[1]から有名人1人の情報を検索し、そのページの有名人全員に関する情報を抽出
-		 * persons_info == nullのとき、そのページにある有名人に関する情報をすべて抽出できたためループを抜ける
-		 */
-
-		while (true) {
-			String[] persons_info = app.TrimTextNext(person_group_info, new String[] { "thumbnailBox", ">" }, "</div>");
-			if (persons_info == null) {
-				break;
+		String category = app.TrimText(app_html,new String[]{"pagenavi","<a"} ,"</div>" );
+		String unshaped_end_page = category.substring(0,category.lastIndexOf("<a"));
+		int end_page = Integer.parseInt(unshaped_end_page.substring(unshaped_end_page.lastIndexOf("\">")+2,unshaped_end_page.lastIndexOf("</a")));
+//		System.out.println(end_page);
+			int num = 1;
+			while(true){
+				int site_url_num = Integer.parseInt(site_url.substring(site_url.lastIndexOf("_")+1,site_url.lastIndexOf(".")));
+				site_url = site_url.replace(String.valueOf(site_url_num), String.valueOf(num));
+				app_html = app.getHTML(site_url);
+				retrievePersons(app_html,person_search_url);
+//				System.out.println(site_url);
+				site_url_num ++;
+				num++;
+				if(end_page < num){
+					break;
+				}
 			}
-			extractPersons(persons_info[0], person_search_url);
-			System.out.println("");
-			person_group_info = persons_info[1];
-		}
 	}
+	
+	/*
+	 * HTMLタグの抽出 抽出したHTMLの文字列から、有名人1人1人の情報は、【<a>】タグの内容を文字列で抽出
+	 *  persons_info !=nullのとき、[0]番目の配列には該当する文字列に有名人1人に関する情報を、 [1]番目の配列にはそれ以降の有名人全員に関する情報をを返す
+	 * while文の中では、1回目のループでperson_group_infoから有名人1人の情報を文字列で検索
+	 * 2回目以降のループで、persons_info[1]から有名人1人の情報を検索し、そのページの有名人全員に関する情報を抽出
+	 * persons_info == nullのとき、そのページにある有名人に関する情報をすべて抽出できたためループを抜ける
+	 */
+		public static String retrievePersons(String html,String search_url){
+			String person_group_info = app.TrimText(html, new String[] { "thumbnailBox_list", ">" }, "<span class=");
+//			 System.out.println(person_group_info);
+			while (true) {
+				String[] persons_info = app.TrimTextNext(person_group_info, new String[] { "thumbnailBox", ">" }, "</div>");
+				if (persons_info == null) {
+					break;
+				}
+				extractPersons(persons_info[0], search_url);
+				System.out.println("");
+				person_group_info = persons_info[1];
+			}
+			return html + search_url;
+		}
 
 	/*
 	 * 1人の有名人に関する情報をさらに細かく抽出 抽出する情報は、【有名人名】、【有名人の画像URL】、【ページのリンクURL】
@@ -107,7 +88,7 @@ public class DemoApp {
 
 	/*
 	 * 抽出した【有名人の画像URL】を画像ファイルとして保存
-	 * 2017年作成する画像ファイルのファイル名変更
+	 * 2017/02/13作成する画像ファイルのファイル名変更
 	 */
 	public static String shapeImg(String img_name, String img_url) {
 		try {
@@ -126,7 +107,7 @@ public class DemoApp {
 
 	/*
 	 * 抽出した【有名人名】、【有名人の画像のファイル名】、【有名人の画像URL】、【ページのリンクURL】を定義したmetadataファイルをjsonファイルとして作成する
-	 * 2017年作成するmetadataファイルのファイル名変更/jsonファイルに定義する情報として、【有名人の画像のファイル名】を追加
+	 * 2017/02/13作成するmetadataファイルのファイル名変更/jsonファイルに定義する情報として、【有名人の画像のファイル名】を追加
 	 */
 	public static String produceText(String name,String img_name, String img_url, String link) {
 		try {
@@ -139,32 +120,5 @@ public class DemoApp {
 		}
 		return null;
 	}
-//	/*2017/2/10追加(開始位置)	*/		
-//	/*	URL存在確認メソッド	*/
-//    public static boolean isExistURL(String urlStr) {
-//        URL url;
-//        int status = 0;
-//        try {
-//            url = new URL(urlStr);
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//            conn.setRequestMethod("HEAD");
-//            conn.connect();
-//            status = conn.getResponseCode();
-//            conn.disconnect();
-//        } catch (MalformedURLException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//
-//        if (status == HttpURLConnection.HTTP_OK) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
-//	/*2017/2/10追加(終了位置)	*/	
 	
 }
