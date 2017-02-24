5d1f1a13e4340621421fc0914db956af1fbd07a8
takana-at <3aun.bmf.397a@gmail.com>
2017-02-24 15:03:55 +0900

    【変更箇所】 コメント箇所の整理

--- src/main/java/sample/app/trainningdata.java
+++ src/main/java/sample/app/trainningdata.java
@@ -131,63 +131,6 @@ public class trainningdata {
 		return null;
 	}
 	
-//	/*	2017/2/22	トレーニング画像データの追加	*/
-//	public static String addTrainImage(String img_imfo,String img_path){
-//		String unencoded_train_img_url = app.TrimText(img_imfo,new String[]{"img","src=\""},"\"");
-//		String train_img_url = "";
-//		String train_img_path = "";
-//		try {
-//			String pre_img_url = unencoded_train_img_url.substring(0,unencoded_train_img_url.lastIndexOf("/")+1);
-//			String post_img_url = unencoded_train_img_url.substring(unencoded_train_img_url.lastIndexOf("/")+1,unencoded_train_img_url.length());
-//			train_img_url = pre_img_url + URLEncoder.encode(post_img_url,"UTF-8");
-//			
-//			System.out.println("\"pre_img_url\" = " + pre_img_url);
-//			System.out.println("\"post_img_url\" = " + post_img_url);
-//			String train_img_name = null;
-//			if(post_img_url.indexOf(".jpg?") != -1){
-//				train_img_name = post_img_url.substring(0,post_img_url.indexOf(".jpg?")+4);
-//			}else if(post_img_url.indexOf(".jpg") == -1 && post_img_url.indexOf(".jpeg") == -1 && post_img_url.indexOf(".png") == -1){
-//				train_img_name = post_img_url + ".jpg";
-//			}else{
-//				train_img_name = post_img_url;
-//			}
-//					
-//			String[] ng_name = {"?","\\","<",">","\\",":","*","\"","|"};
-//			for(int i = 0; i < ng_name.length; i++){
-//				if(train_img_name.indexOf(ng_name[i]) > -1){
-//					train_img_name = train_img_name.replace(ng_name[i], "");
-//				}
-//			}
-//	/*
-//			char[] ng_str = train_img_name.toCharArray();
-//			for(int i = 0; i < ng_str.length; i++){
-//				if(String.valueOf(ng_str[i]).getBytes().length > 1 ){
-//					System.out.println(ng_str[i]);
-//					System.out.println(i);
-//					train_img_name = train_img_name.replace(String.valueOf(ng_str[i]),String.valueOf(i));
-//				}
-//			}
-//	*/					
-//			System.out.println("\"train_img_url\" = " + train_img_url);
-////			System.out.println("\"train_img_name\" = " + train_img_name);
-//			train_img_path = "./trainImage/" + img_path + "/" + train_img_name;
-////			System.out.println("\"train_img_path\" = " + train_img_path);
-//			app.addImage(train_img_url,train_img_path);
-//		} catch (UnsupportedEncodingException e) {
-//			e.printStackTrace();
-//		}
-//		return train_img_url + train_img_path;
-//	}
-	
-	
-
-	
-	
-	
-	
-	
-	
-	
 //	/*
 //	 * HTMLタグの抽出 抽出したHTMLの文字列から、有名人1人1人の情報は、【<a>】タグの内容を文字列で抽出
 //	 */
